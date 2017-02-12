40c3230599aa4c4d6f018dde6a55ab2390dbb061
Jimmy Liu <jimmycloud@gmail.com>
2017-02-11 19:40:47 -0800

    Update intersection_of_two_linked_lists.cpp

--- Cpp/160. Intersection of Two Linked Lists/intersection_of_two_linked_lists.cpp
+++ Cpp/160. Intersection of Two Linked Lists/intersection_of_two_linked_lists.cpp
@@ -39,8 +39,8 @@ class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
         ListNode *p1 = headA, *p2 = headB;
-        if (p1==NULL || p2==NULL) return NULL;
-        while(p1!=NULL && p2!=NULL && p1!=p2)
+        if (p1 == NULL || p2 == NULL) return NULL;
+        while(p1 != NULL && p2 != NULL && p1 != p2)
         {
             p1 = p1->next;
             p2 = p2->next;
