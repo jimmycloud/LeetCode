74916cb5b43699f8c058e6c5c84ec1fd6ea22891
Jimmy Liu <jimmycloud@gmail.com>
2017-02-12 22:45:33 -0800

    Update length_of_last_word.java

--- Cpp/58. Length of Last Word/length_of_last_word.java
+++ Cpp/58. Length of Last Word/length_of_last_word.java
@@ -1,3 +1,21 @@
+// Source : https://leetcode.com/problems/length-of-last-word/
+// Author : Jimmy Liu
+// Date   : 2017-02-12
+// Reference:
+// Idea: 
+
+/*
+Given a string s consists of upper/lower-case alphabets and empty space characters ' ', return the length of last word in the string.
+
+If the last word does not exist, return 0.
+
+Note: A word is defined as a character sequence consists of non-space characters only.
+
+For example, 
+Given s = "Hello World",
+return 5.
+*/
+
 public class Solution {
     public int lengthOfLastWord(String s) {
         int n = s.length();
